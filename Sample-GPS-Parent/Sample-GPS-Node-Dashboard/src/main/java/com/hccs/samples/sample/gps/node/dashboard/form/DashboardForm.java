ab108b591f8f336ecdbb7865c2a6a0df27124411
Maikcel Escalante II <maikcelescalanteii@gmail.com>
2019-09-03 14:15:50 +0800

    Deleted a dashboard created separately in the boot project.

--- Sample-GPS-Parent/Sample-GPS-Node-Dashboard/src/main/java/com/hccs/samples/sample/gps/node/dashboard/form/DashboardForm.java
+++
@@ -1,7 +0,0 @@
-package com.hccs.samples.sample.gps.node.dashboard.form;
-
-import com.hccs.samples.sample.gps.core.interfaces.Showable;
-
-public interface DashboardForm extends Showable {
-
-}
--- Sample-GPS-Parent/Sample-GPS-Node-Dashboard/src/main/java/com/hccs/samples/sample/gps/node/dashboard/gateway/DashboardGateway.java
+++
@@ -1,8 +0,0 @@
-package com.hccs.samples.sample.gps.node.dashboard.gateway;
-
-import java.util.List;
-
-public interface DashboardGateway {
-
-	public List<Object> getAllObjectsByFilter(List<String> filters);
-}
